2837824b9d8394e9d78191966568ea0a97c35e30
Mahfud Prasetyo <37892922+farma98@users.noreply.github.com>
2022-06-11 13:29:26 +0700

    Mengerjakan tugas promise

--- promise.js
+++ promise.js
@@ -1,7 +1,21 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (hasil) => {
+  const hasilTheaterIXX = await promiseTheaterIXX();
+  const hasilTheaterVGC = await promiseTheaterVGC();
+  const hasilSetelahMenonton = [...hasilTheaterIXX, ...hasilTheaterVGC];
+
+  await new Promise((r) => setTimeout(r, 200));
+
+  return new Promise((resolve) => {
+    const hasilSetelahMenontonDariTheater = hasilSetelahMenonton.filter(
+      (item) => item.hasil === hasil
+    );
+
+    return resolve(hasilSetelahMenontonDariTheater.length);
+  });
+};
 
 module.exports = {
   promiseOutput,
